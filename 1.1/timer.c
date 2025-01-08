0640a3fa401917af65b082544b0bea7689079fd6
angh-el <anghel.marius15@gmail.com>
2025-01-08 13:10:20 +0000

    Implemented a simple timer interrupt

---
+++ 1.1/timer.c
@@ -0,0 +1,26 @@
+#include "libs/printf.h"
+#include "libs/util.h"
+#include "idt.h"
+#include "drivers/screen.h"
+#include "timer.h"
+
+uint64_t ticks;
+const uint32_t freq = 100;
+
+void onIrq0(struct interrupt_register *regs){
+    ticks += 1;
+    printf("hello ");
+}
+
+void init_timer(){
+    ticks = 0;
+    irq_install_handler(0,&onIrq0);
+
+    //119318.16666 Mhz
+    uint32_t divisor = 1193180/freq;
+
+    //0011 0110
+    port_byte_out(0x43,0x36);
+    port_byte_out(0x40,(uint8_t)(divisor & 0xFF));
+    port_byte_out(0x40,(uint8_t)((divisor >> 8) & 0xFF));
+}
---
+++ 1.1/timer.h
@@ -0,0 +1,2 @@
+void init_timer();
+void onIrq0(struct interrupt_register *regs);
